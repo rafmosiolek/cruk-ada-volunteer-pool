64cf32b47b980b240bd5e8640fd61a45cdfd2eff
Karolina Zdziarska <Karolina.Zdziarska@britishland.com>
2020-01-30 14:28:25 +0000

    added main login page and event page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Redirect from "./index";
 import React from "react";
 import logo from "./images/CRUK-logo.png";
 import "./App.css";
+import GoogleAuth from "./GoogleAuth";
+import Event from "./Event";
+import google from "./images/google-icon.png";
+import facebook from "./images/facebook-icon.png";
 
 function App() {
   return (
@@ -10,8 +15,22 @@ function App() {
       </header>
       <div className="login-container">
         <p className="login-label">Login:</p>
-        <div className="login-icon">Facebook</div>
-        <div className="login-icon">Google</div>
+        <div className="login-icon">
+          <img
+            src={facebook}
+            onClick={() => {
+              Redirect(<Event />);
+            }}
+          ></img>
+        </div>
+        <div className="login-icon">
+          <img
+            src={google}
+            onClick={() => {
+              Redirect(<Event />);
+            }}
+          ></img>
+        </div>
       </div>
     </div>
   );
---
+++ src/Event.js
@@ -0,0 +1,73 @@
+import React from "react";
+import "./Event.css";
+import Redirect from "./index";
+import App from "./App";
+import back from "./images/back-arrow.png";
+import image from "./images/event-example-image.jpg";
+
+function Event() {
+  return (
+    <div className="Event">
+      <div className="subpage-header">
+        <img
+          src={back}
+          onClick={() => {
+            Redirect(<App />);
+          }}
+        ></img>
+        <div class="subpage-header-title">Event Name</div>
+      </div>
+      <div className="subpage-container">
+        <img src={image} className="event-image"></img>
+        <div class="event-details">
+          <div class="event-location">Location: location</div>
+          <div class="event-date">Date: date</div>
+        </div>
+        <div class="event-why">Why Volunteer?</div>
+        <div class="event-description">
+          700,000 runners, walkers, swimmers and sliders are limbering up to
+          join the fight against cancer but they can’t cross the finish line
+          without the passion and energy of volunteers like you. We simply
+          couldn't do it without the support of our amazing volunteers. You can
+          make a real difference to our research!
+        </div>
+
+        <button
+          class="volunteer-button"
+          onClick={() => {
+            toggleButtonDisplay("volunteer");
+          }}
+        >
+          Volunteer
+        </button>
+        <div class="button-container">
+          <button
+            class="cancel-button"
+            onClick={() => {
+              toggleButtonDisplay("cancel");
+            }}
+          >
+            Cancel
+          </button>
+          <button class="qr-button">QR</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function toggleButtonDisplay(toggle) {
+  switch (toggle) {
+    case "volunteer":
+      document.querySelector(".volunteer-button").style.display = "none";
+      document.querySelector(".cancel-button").style.display = "table-cell";
+      document.querySelector(".qr-button").style.display = "table-cell";
+      break;
+    case "cancel":
+      document.querySelector(".volunteer-button").style.display = "block";
+      document.querySelector(".cancel-button").style.display = "none";
+      document.querySelector(".qr-button").style.display = "none";
+  }
+}
+
+export default Event;
