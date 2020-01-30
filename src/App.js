60a564ebb3cb8d8e2e505fdee8d5cf58cf6ed382
Karolina Zdziarska <Karolina.Zdziarska@britishland.com>
2020-01-30 10:52:40 +0000

    initial front end setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./images/CRUK-logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="homepage-logo" alt="logo" />
       </header>
+      <div className="login-container">
+        <p className="login-label">Login:</p>
+        <div className="login-icon">Facebook</div>
+        <div className="login-icon">Google</div>
+      </div>
     </div>
   );
 }
